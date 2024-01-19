c0bff7e0a98cff6949b3b3c5a39bd67ad1e22e0a
Saryace <saryace@gmail.com>
2024-01-19 13:24:20 -0300

    delete delayedAssing

--- data/usda_polygons.R
+++
@@ -1,7 +0,0 @@
-delayedAssign("ggsoiltexture", local({
-  if (requireNamespace("tibble", quietly = TRUE)) {
-    tibble::as_tibble(ggsoiltexture:::usda_polygons)
-  } else {
-    ggsoiltexture:::usda_polygons
-  }
-}))
