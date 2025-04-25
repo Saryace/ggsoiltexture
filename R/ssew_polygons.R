c206b6622fde2f708c075e67330fa585d326e150
Tom Bishop <tombishopemail@gmail.com>
2025-04-25 17:03:40 +0100

    Add support for UK Soil Classification
    The Soil Survey of England and Wales uses classifications defined in Natural England Technical Information Note TIN037: Soil Texture. This adds a soil texture triangle for this scheme.

---
+++ R/ssew_polygons.R
@@ -0,0 +1,5 @@
+#' @title Soil Survey of England and Wales soil texture polygons (ssew_polygons)
+#' @description Data describing polygons for each soil textural class following Natural England TIN037
+#' @format dataframe-tibble
+#' @source Natural England TIN037
+"ssew_polygons"
