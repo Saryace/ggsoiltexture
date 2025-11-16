863d5566d74ee8179f04c82df09b0e7b076cd202
obrl-mwlr <obrienl@landcareresearch.co.nz>
2025-11-16 19:12:26 +1300

    Add support for NZ texture groups
    also tidy up existing NZ dataset

--- data-raw/nz_polygons.R
+++ data-raw/nz_polygons.R
@@ -1,12 +1,8 @@
 library(tidyverse)
 nz_polygons <- read.csv("data-raw/NZTT.csv") %>%
-  mutate(x = 0.5 * CLAY + SILT,
-         y = CLAY,
-         label = NAME) %>%
   mutate(
     label = case_when(
       label == "Sandy Clay Loam" ~ "Sandy\nClay Loam",
       TRUE ~ as.character(label)
     )
   )
-
---
+++ data-raw/nzg_polygons.R
@@ -0,0 +1,1 @@
+nzg_polygons <- read.csv("data-raw/NZ_groups.csv")
