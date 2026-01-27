42dde6574efa2b90c942ac30170c6686de051ed1
MrnWllrdt <moreenwillaredt@posteo.de>
2026-01-27 15:25:03 -0800

    update reference for German soil texture

--- R/german_polygons.R
+++ R/german_polygons.R
@@ -1,5 +1,5 @@
-#' @title USDA soil texture polygons (usda_polygons)
+#' @title German soil texture polygons (german_polygons)
 #' @description Data describing polygons for each soil textural class
 #' @format dataframe-tibble
-#' @source USDA and ggtern data(USDA)
+#' @source The German soil texture classification is described in the German Manual of soil mapping: Kartieranleitung Bodenschätzung (KA5) (Ad-hoc-Arbeitsgruppe Boden 2005). Ad-hoc-AG Boden (2005). Bodenkundliche Kartieranleitung (German Soil Survey Guideline). 5th Edition (KA5). Hannover.
 "german_polygons"
